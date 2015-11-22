a5bc2a0e601a8c62b7f6d92ba5994648f0618d70
acedened <acedened@outlook.com>
2015-11-22 20:27:19 +0300

    Created minimal project

---
+++ Win2ch/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+using Win2ch.ViewModels;
+using Windows.UI.Xaml.Controls;
+
+namespace Win2ch.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Disabled;
+        }
+
+        // strongly-typed view models enable x:bind
+        public MainPageViewModel ViewModel => this.DataContext as MainPageViewModel;
+    }
+}
