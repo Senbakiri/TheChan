3cf34741b03c64f741c690151ad9c24c0e0c5b70
Vadim Novoseltsev <acedened@ya.ru>
2016-02-11 16:34:55 +0300

    Исправлен вылет при усердном желании показать MessageDialog

--- Win2ch/Models/Utils.cs
+++ Win2ch/Models/Utils.cs
@@ -5,32 +5,44 @@ using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Core;
 using Windows.UI.Popups;
 using HtmlAgilityPack;
 using Win2ch.Models.Exceptions;
 
 namespace Win2ch.Models {
     public static class Utils {
+        private static IAsyncOperation<IUICommand> _messageDialogCommand;
+
         public static async Task ShowConnectionError(COMException exception, string title) {
-            var dialog = new MessageDialog(
+            await ShowDialog(
                 $"Проверьте подключение к интернету. Код ошибки: 0x{exception.HResult:X}.",
                 title);
-            await dialog.ShowAsync();
         }
 
         public static async Task ShowHttpError(HttpException exception, string title) {
-            var dialog = new MessageDialog(
-                $"Код ошибки: {exception.Code} {exception.Message}.",
+            await ShowDialog($"Код ошибки: {exception.Code} {exception.Message}.",
                 title);
-            await dialog.ShowAsync();
         }
 
         public static async Task ShowOtherError(Exception exception, string title) {
-            var dialog = new MessageDialog(
-                exception.Message, title);
-            await dialog.ShowAsync();
+            await ShowDialog(exception.Message, title);
         }
 
+        private static async Task ShowDialog(string text, string title) {
+            try {
+                _messageDialogCommand?.Cancel();
+                var dialog = new MessageDialog(text, title);
+                _messageDialogCommand = dialog.ShowAsync();
+                await _messageDialogCommand;
+                _messageDialogCommand = null;
+            } catch (UnauthorizedAccessException) {
+                _messageDialogCommand = null;
+            } catch (TaskCanceledException) {
+                _messageDialogCommand = null;
+            }
+        }
 
         public static string RemoveHtml(string html) {
             var doc = new HtmlDocument();
