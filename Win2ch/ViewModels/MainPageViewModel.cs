2c050bc121c24c50a4dca2565607eb4b22c3a86a
Vadim Novoseltsev <acedened@ya.ru>
2016-02-13 23:11:44 +0300

    Добавление зацензуренных досок в общий список при их посещении

--- Win2ch/ViewModels/MainPageViewModel.cs
+++ Win2ch/ViewModels/MainPageViewModel.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Windows.Storage;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Win2ch.Models;
@@ -15,14 +18,18 @@ namespace Win2ch.ViewModels {
 
         private StorageService<Board> FavoriteBoards { get; } = FavoritesService.Instance.Boards;
 
-        public MainPageViewModel() {
-            Categories = new ObservableCollection<Category>();
-        }
-
         public ObservableCollection<Category> Categories { get; set; }
 
         private List<Category> HiddenCategories { get; } = new List<Category>();
 
+        private StorageService<string> AllowedBoards { get; }
+
+        public MainPageViewModel() {
+            Categories = new ObservableCollection<Category>();
+            AllowedBoards = new StorageService<string>(ApplicationData.Current.RoamingFolder,
+                "AllowedBoards.json");
+        }
+        
         public async void LoadBoards() {
             Categories.Clear();
 
@@ -35,7 +42,7 @@ namespace Win2ch.ViewModels {
             try {
                 categories = await new ApiBoardsProvider().GetCategories();
             } catch (HttpException e) {
-                var dialog = new MessageDialog("Код ошибки: " + (int)e.Code, "Не удалось получить список досок");
+                var dialog = new MessageDialog("Код ошибки: " + e.Code, "Не удалось получить список досок");
                 dialog.Commands.Add(new UICommand("Повторить", _ => LoadBoards()));
                 dialog.Commands.Add(new UICommand("Закрыть"));
                 dialog.Commands.Add(new UICommand("Выход", _ => Application.Current.Exit()));
@@ -52,12 +59,37 @@ namespace Win2ch.ViewModels {
                 // censorship
                 if (category.Name != "Взрослым")
                     Categories.Add(category);
-                else
+                else {
                     HiddenCategories.Add(category);
+                    var cat = await CreateCategoryWithAllowedBoards(category);
+                    if (cat.Count > 0)
+                        Categories.Add(cat);
+                }
             }
         }
 
-        public void NavigateToBoard(Board board) {
+        private async Task<Category> CreateCategoryWithAllowedBoards(Category baseCategory) {
+            List<string> boards;
+
+            try {
+                boards = (await AllowedBoards.GetItems()).ToList();
+            } catch (Exception) {
+                return baseCategory;
+            }
+
+            return new Category(baseCategory.Where(b => boards.Contains(b.Id))) {
+                Name = baseCategory.Name
+            };
+        }
+
+        public async void NavigateToBoard(Board board) {
+            try {
+                if (HiddenCategories.Any(c => c.Contains(board)))
+                    await AllowedBoards.Add(board.Id);
+            } catch {
+                // it's okay
+            }
+
             NavigationService.Navigate(typeof(BoardPage), board);
         }
     }
