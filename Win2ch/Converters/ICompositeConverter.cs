ad8dd353cc5363d5de04b57ec494ae470312acdd
Vadim Novoseltsev <acedened@ya.ru>
2016-02-13 20:08:43 +0300

    Switch Converter

---
+++ Win2ch/Converters/ICompositeConverter.cs
@@ -0,0 +1,8 @@
+﻿using Windows.UI.Xaml.Data;
+
+namespace Win2ch.Converters {
+    public interface ICompositeConverter : IValueConverter {
+        IValueConverter PostConverter { get; set; }
+        object PostConverterParameter { get; set; }
+    }
+}
