0046ca148380a4dc152e33952419284ef6b692d8
Vadim Novoseltsev <acedened@ya.ru>
2016-02-21 10:57:00 +0300

    Очистка

--- Win2ch/Views/SettingsPage.xaml.cs
+++ Win2ch/Views/SettingsPage.xaml.cs
@@ -1,17 +1,11 @@
-﻿using Windows.UI.Xaml.Controls;
-using Win2ch.ViewModels;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+﻿using Win2ch.ViewModels;
 
 namespace Win2ch.Views {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class SettingsPage : Page {
+    public sealed partial class SettingsPage {
         public SettingsViewModel ViewModel { get; private set; }
 
         public SettingsPage() {
-            this.InitializeComponent();
+            InitializeComponent();
             DataContextChanged += (s, e) => ViewModel = DataContext as SettingsViewModel;
         }
     }
