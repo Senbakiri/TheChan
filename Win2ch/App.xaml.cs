696e3f75630d6c4357c705386688fff68caa80d3
Vadim Novoseltsev <acedened@ya.ru>
2016-03-08 12:44:17 +0300

    Initial project

---
+++ Win2ch/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Caliburn.Micro;
+using Win2ch.ViewModels;
+
+namespace Win2ch
+{
+    sealed partial class App {
+
+        private WinRTContainer _container;
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void Configure() {
+            _container = new WinRTContainer();
+            _container.RegisterWinRTServices();
+            _container.Singleton<ShellViewModel>();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args) {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key) {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service) {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance) {
+            _container.BuildUp(instance);
+        }
+    }
+}
---
+++ Win2ch/ViewModels/ShellViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Win2ch.ViewModels {
+    class ShellViewModel : IConductActiveItem {
+        private object _ActiveItem;
+
+        public IEnumerable GetChildren() {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyOfPropertyChange([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Refresh() {
+            throw new NotImplementedException();
+        }
+
+        public bool IsNotifying { get; set; }
+
+        public void ActivateItem(object item) {
+            throw new NotImplementedException();
+        }
+
+        public void DeactivateItem(object item, bool close) {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler<ActivationProcessedEventArgs> ActivationProcessed;
+
+        public object ActiveItem {
+            get { return _ActiveItem; }
+            set {
+                if (ActiveItem == value)
+                    return;
+                _ActiveItem = value;
+                NotifyOfPropertyChange();
+            }
+        }
+    }
+}
