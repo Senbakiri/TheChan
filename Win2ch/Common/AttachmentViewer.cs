171f9c1bd1347a9f90286c1bda0c0bda8ed2f3f8
Vadim Novoseltsev <acedened@ya.ru>
2016-04-02 12:53:14 +0300

    IAttachmentViewer

---
+++ Win2ch/Common/AttachmentViewer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Models;
+
+namespace Win2ch.Common {
+    public class AttachmentViewer : IAttachmentViewer {
+        private IList<Attachment> AllAttachments { get; set; }
+        private Attachment CurrentAttachment { get; set; }
+        private IShell Shell { get; }
+
+        public AttachmentViewer(IShell shell) {
+            Shell = shell;
+        }
+
+        public void View(Attachment currentAttachment, IEnumerable<Attachment> allAttachments) {
+            AllAttachments = new List<Attachment>(allAttachments);
+            CurrentAttachment = currentAttachment;
+            switch (currentAttachment.Type) {
+                default:
+                    OpenImage();
+                    break;
+            }
+        }
+
+        private void OpenImage() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Win2ch/Common/IAttachmentViewer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using Core.Models;
+
+namespace Win2ch.Common {
+    public interface IAttachmentViewer {
+        void View(Attachment currentAttachment, IEnumerable<Attachment> allAttachments);
+    }
+}
